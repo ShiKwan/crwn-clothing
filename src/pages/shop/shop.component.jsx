f1f5c785daf5df2c6bfd7bbdb06f0a9c7c4720be
Shi-Kwan <38438280+1704043f@users.noreply.github.com>
2020-12-27 13:14:29 -0500

    moved directory and shop data into reducer, and created respective selectors and updated corresponding component for ux flow for directory and shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import CollectionOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match, fetchCollectionsStart }) => {
-
-    useEffect( () => {
-        fetchCollectionsStart();
-    }, [fetchCollectionsStart])
-
-    return(
+const ShopPage= ({match}) =>
+    (
         <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+            <Route exact path={`${match.path}`} component={CollectionOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div>
     )
-};
 
-
-
-const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
-});
-export default connect(null, mapDispatchToProps)(ShopPage);
+export default ShopPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,14 +7,8 @@ import createSagaMiddleware from 'redux-saga'
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [sagaMiddleware];
-
-if (process.env.NODE_ENV === 'development'){
-    middlewares.push(logger);
-}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(rootSaga)
 export const persistor = persistStore(store);
 
-export default {store, persistor};
+export default { store, persistor };
